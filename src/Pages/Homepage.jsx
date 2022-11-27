d564bf238388141907fc34fb36edeac2374682e6
nethanel10 <nethanelbedok@gmail.com>
2022-11-27 11:34:20 +0200

    Homepage page style

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -3,7 +3,9 @@ function Homepage(){
 
     return(
         <div className="Homepage">
-
+<h1 className='Homeheader'>BUY Online</h1> 
+<h1 className='Homeheader'>Eat slowly</h1>
+<button className='Order-button'>Order now</button>
         </div>
     )
 }
